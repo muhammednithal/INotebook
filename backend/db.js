a5fdd8a2b818ce67c1a91d8dd2b87a8e346724c8
muhammednithal <muhammednithalb14@gmail.com>
2023-07-18 23:03:56 +0530

    bcrypt

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ const mongoose=require('mongoose');
 
 
 const  connectToMongo=async() =>{
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-  console.log("jei")
+  await mongoose.connect('mongodb://127.0.0.1:27017/inotebookDB');
+  console.log("connected to mongodb")
   
 }
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,43 +1,68 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post("/", [
-  // Validate email
-  body('email')
-    .isEmail().withMessage('Invalid email format')
-    .custom((value) => {
-      return User.findOne({ email: value }).then(user => {
+const JWT_SECRET="heloonithal"
+
+//create a user using :POST "/api/auth/createuser".no login required
+router.post(
+  "/createuser",
+  [
+    // Validate email
+    body("email")
+      .isEmail()
+      .withMessage("Invalid email format")
+      .custom(async (value) => {
+        const user = await User.findOne({ email: value });
         if (user) {
-          return Promise.reject('Email already exists');
+          return Promise.reject("Email already exists");
         }
+      }),
+
+    // Validate name
+    body("name")
+      .isLength({ min: 5 })
+      .withMessage("Name must be at least 5 characters"),
+
+    // Validate password
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters"),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const salt=await bcrypt.genSalt(10)
+      const secPass=await bcrypt.hash(req.body.password,salt)
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass
       });
-    }),
-
-  // Validate name
-  body('name')
-    .isLength({ min: 5 }).withMessage('Name must be at least 5 characters'),
-
-  // Validate password
-  body('password')
-    .isLength({ min: 5 }).withMessage('Password must be at least 5 characters')
-], (req, res) => {
-  // Check for validation errors
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authToken=jwt.sign(data,JWT_SECRET)
+      
+      res.json({authToken});
+    } catch {
+      (error) => {
+        console.error(error.message);
+        res
+          .status(500)
+          .json({ error: "An error occurred while saving the user" });
+      };
+    }
   }
-
-  console.log(req.body);
-  const user = new User(req.body);
-  user.save()
-    .then(() => {
-      res.json(req.body);
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'An error occurred while saving the user' });
-    });
-});
+);
 
 module.exports = router;
