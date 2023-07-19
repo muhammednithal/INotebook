f8eccdf6d0dd4b1b8f30924fe8c9217419b7a868
muhammednithal <muhammednithalb14@gmail.com>
2023-07-19 13:34:43 +0530

    post operation

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -53,4 +53,61 @@ router.post(
     }
   }
 );
+
+//ROUTE 3: Update an existing Note :POST "/api/auth/updatenote". login required
+router.put(
+  "/updatenote/:id",
+  fetchUser,
+  [
+    // Validate name
+    body("title")
+      .isLength({ min: 3 })
+      .withMessage("Title must be at least 3 characters"),
+
+    // Validate password
+    body("description")
+      .isLength({ min: 5 })
+      .withMessage("Description must be at least 5 characters"),
+  ],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const { title, description, tag } = req.body;
+      //create a new note object
+      const newnote = {};
+      if (title) {
+        newnote.title = title;
+      }
+      if (description) {
+        newnote.description = description;
+      }
+      if (tag) {
+        newnote.tag = tag;
+      }
+
+      //find the note to be updated and update it
+      let note = await Note.findById(req.params.id);
+      if (!note) {
+        res.status(404).sent("Not Found");
+      }
+
+      if (note.user.toString() !== req.user.id) {
+        res.status(401).send("Not Allowed");
+      }
+      note = await Note.findByIdAndUpdate(
+        req.params.id,
+        { $set: newnote },
+        { new: true }
+      );
+      res.json(note);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("internal server error");
+    }
+  }
+);
 module.exports = router;
