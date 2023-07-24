5de1cdb1d4b4e8c1abed3909a5c7b46a2d4e4f04
muhammednithal <muhammednithalb14@gmail.com>
2023-07-24 13:55:08 +0530

    homepage setup

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,15 @@
+import React from "react";
+import noteContext from "./noteContext";
+
+
+const NoteState=(props)=>{
+
+return(
+    <noteContext.Provider value={{}}>
+        {props.children}
+    </noteContext.Provider>
+)
+}
+
+
+export default NoteState;
